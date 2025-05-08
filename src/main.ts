f83d26e7757c3d46a03645e7a68401f46e492893
LucasPontees <eulpontes@gmail.com>
2025-05-08 16:02:40 -0400

    Update dependencies in package.json and package-lock.json to include @nestjs/swagger and @scalar/nestjs-api-reference, and configure Swagger documentation in main.ts for API service.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('api-service')
+    .setDescription('API para objetos de estudo')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  app.use('/docs', apiReference({
+    content: document,
+    theme: 'solarized', // 'default' | 'dark' | 'purple' | 'solarized'
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
