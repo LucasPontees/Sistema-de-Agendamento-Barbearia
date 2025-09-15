cbdaf3686b8fe92b98c7c92d2d533e2be156f439
LucasPontees <eulpontes@gmail.com>
2025-09-14 23:56:54 -0400

    feat: integrated a form login with a api

--- frontend/src/app/(public)/login/page.tsx
+++ frontend/src/app/(public)/login/page.tsx
@@ -1,134 +1,145 @@
-
-'use client';
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/components/ui/use-toast";
-import NextNavbar from '@/components/NextNavbar';
-import NextFooter from '@/components/NextFooter';
-import { api } from '@/http/api'; // Adjust the import based on your API service setup
+import NextNavbar from "@/components/NextNavbar";
+import NextFooter from "@/components/NextFooter";
+import { api } from "@/http/api"; // Adjust the import based on your API service setup
 
 const Login: React.FC = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [carregando, setCarregando] = useState(false);
+  const router = useRouter();
+  const [erro, setErro] = useState("");
+  const [mensagem, setMensagem] = useState("");
 
-    const [form, setForm] = useState({
-        login: '',
-        password: ''
-    });
-    const [isLoading, setIsLoading] = useState(false);
-    const [carregando, setCarregando] = useState(false);
-    const router = useRouter();
-    const [erro, setErro] = useState('');
-    const [mensagem, setMensagem] = useState("");
+  const { toast } = useToast();
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
 
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!form.email || !form.password) return;
+    setCarregando(true);
+    setErro("");
+    try {
+      await api.post("auth/login", {
+        json: { email: form.email, password: form.password },
+      });
 
-    const { toast } = useToast();
-
-
-
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setForm({ ...form, [e.target.name]: e.target.value });
+      setMensagem("Login realizado com sucesso!");
+      setTimeout(() => {
+        setTimeout(() => {
+          router.push("/dashboard");
+        }, 1000);
+      }, 1000);
+    } catch (error) {
+      setMensagem("Credenciais inválidas!");
+      setTimeout(() => setMensagem(""), 1000);
+    } finally {
+      setCarregando(false);
     }
+  }
 
-    async function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        if (!form.login || !form.password) return;
-        setCarregando(true);
-        setErro('');
-        try {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NextNavbar isAuthenticated={false} />
 
-            await api.post("auth/login", {
-                json: { login: form.login, password: form.password },
-            });
+      <main className="flex-grow flex items-center justify-center py-12">
+        <div className="container px-4">
+          <Card className="mx-auto max-w-md">
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-2xl font-bold">Login</CardTitle>
+              <CardDescription>
+                Digite suas credenciais para acessar sua conta
+              </CardDescription>
+            </CardHeader>
 
-            setMensagem("Login realizado com sucesso!");
-            setTimeout(() => {
-                setTimeout(() => {
-                    router.push("/dashboard");
-                }, 1000);
-            }, 1000);
-        } catch (error) {
-            setMensagem("Credenciais inválidas!");
-            setTimeout(() => (setMensagem("")), 1000);
-        } finally {
-            setCarregando(false);
-        }
-    }
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <div className="space-y-4">
+                  <div className="space-y-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      required
+                      name="email"
+                      placeholder="email"
+                      autoComplete="off"
+                      value={form.email}
+                      onChange={handleChange}
+                    />
+                  </div>
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <NextNavbar isAuthenticated={false} />
+                  <div className="space-y-2">
+                    <div className="flex items-center justify-between">
+                      <Label htmlFor="password">Senha</Label>
+                      <Link
+                        href="/forgot-password"
+                        className="text-sm text-barber-gold hover:underline"
+                      >
+                        Esqueceu a senha?
+                      </Link>
+                    </div>
+                    <Input
+                      id="password"
+                      type="password"
+                      required
+                      placeholder="••••••••"
+                      name="password"
+                      value={form.password}
+                      onChange={handleChange}
+                    />
+                  </div>
 
-            <main className="flex-grow flex items-center justify-center py-12">
-                <div className="container px-4">
-                    <Card className="mx-auto max-w-md">
-                        <CardHeader className="space-y-1">
-                            <CardTitle className="text-2xl font-bold">Login</CardTitle>
-                            <CardDescription>
-                                Digite suas credenciais para acessar sua conta
-                            </CardDescription>
-                        </CardHeader>
-
-                        <CardContent>
-                            <form onSubmit={handleSubmit}>
-                                <div className="space-y-4">
-                                    <div className="space-y-2">
-                                        <Label htmlFor="login">Email</Label>
-                                        <Input
-                                            id="login"
-                                            type="login"
-                                            required
-                                            name="login"
-                                            placeholder="Login"
-                                            value={form.login}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-
-                                    <div className="space-y-2">
-                                        <div className="flex items-center justify-between">
-                                            <Label htmlFor="password">Senha</Label>
-                                            <Link href="/forgot-password" className="text-sm text-barber-gold hover:underline">
-                                                Esqueceu a senha?
-                                            </Link>
-                                        </div>
-                                        <Input
-                                            id="password"
-                                            type="password"
-                                            required
-                                            placeholder="••••••••"
-                                            name="password"
-                                            value={form.password}
-                                            onChange={handleChange}
-                                        />
-                                    </div>
-
-                                    <Button type="submit" className="w-full bg-barber-gold hover:bg-barber-gold/90 text-black" disabled={isLoading}>
-                                        {isLoading ? "Entrando..." : "Entrar"}
-                                    </Button>
-                                </div>
-                            </form>
-                        </CardContent>
-
-                        <CardFooter className="flex flex-col space-y-4">
-                            <div className="text-sm text-center text-muted-foreground">
-                                Não tem uma conta?{" "}
-                                <Link href="/register" className="text-barber-gold hover:underline">
-                                    Registre-se
-                                </Link>
-                            </div>
-                        </CardFooter>
-                    </Card>
+                  <Button
+                    type="submit"
+                    className="w-full bg-barber-gold hover:bg-barber-gold/90 text-black"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? "Entrando..." : "Entrar"}
+                  </Button>
                 </div>
-            </main>
+              </form>
+            </CardContent>
 
-            <NextFooter />
+            <CardFooter className="flex flex-col space-y-4">
+              <div className="text-sm text-center text-muted-foreground">
+                Não tem uma conta?{" "}
+                <Link
+                  href="/register"
+                  className="text-barber-gold hover:underline"
+                >
+                  Registre-se
+                </Link>
+              </div>
+            </CardFooter>
+          </Card>
         </div>
-    );
+      </main>
+
+      <NextFooter />
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/components/NextNavbar.tsx
+++ frontend/src/components/NextNavbar.tsx
@@ -1,139 +1,230 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { UserCircle2, Menu, X } from 'lucide-react';
-import Link from 'next/link';
+"use client";
+import React, { useEffect, useState } from "react";
+import { UserCircle2, Menu, X } from "lucide-react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
-import { useIsMobile } from '@/hooks/use-mobile';
-import { useRouter } from 'next/navigation';
-import { api } from '@/http/api';
+import { useIsMobile } from "@/hooks/use-mobile";
+import { useRouter } from "next/navigation";
+import { api } from "@/http/api";
 
-function useAuth() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+// function useAuth() {
+//     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                const data = await api.get('auth/check').json<{ isAuthenticated: boolean }>();
-                setIsAuthenticated(data.isAuthenticated);
-            } catch (err) {
-                console.error('Erro ao verificar autenticação:', err);
-                setIsAuthenticated(false);
-            }
-        };
+//     useEffect(() => {
+//         const checkAuth = async () => {
+//             try {
+//                 const data = await api.get('auth/check').json<{ isAuthenticated: boolean }>();
+//                 setIsAuthenticated(data.isAuthenticated);
+//             } catch (err) {
+//                 console.error('Erro ao verificar autenticação:', err);
+//                 setIsAuthenticated(false);
+//             }
+//         };
 
-        checkAuth();
-    }, []);
+//         checkAuth();
+//     }, []);
 
-    return isAuthenticated;
-}
+//     return isAuthenticated;
+// }
 
 interface HeaderProps {
-    isAuthenticated?: boolean;
-    onLogout?: () => void;
+  isAuthenticated?: boolean;
+  onLogout?: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ onLogout }) => {
-    const [menuOpen, setMenuOpen] = React.useState(false);
-    const isAuthenticated = useAuth(); // aqui você pega o estado atual via API
-    const isMobile = useIsMobile();
-    const { toast } = useToast();
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  //   const isAuthenticated = useAuth(); // aqui você pega o estado atual via API
+  const isMobile = useIsMobile();
+  const { toast } = useToast();
 
-    const router = useRouter();
+  const router = useRouter();
 
+  const handleLogout = async () => {
+    try {
+      await api.post("auth/logout"); // Faz o logout no backend
 
+      router.push("/login"); // Redireciona após logout
+      toast({ title: "Desconectado com sucesso!" });
+    } catch (error) {
+      console.error("Erro ao fazer logout", error);
+    }
+  };
 
-    const handleLogout = async () => {
-        try {
-            await api.post('auth/logout'); // Faz o logout no backend
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
-            router.push('/login'); // Redireciona após logout
-            toast({ title: 'Desconectado com sucesso!' });
-        } catch (error) {
-            console.error('Erro ao fazer logout', error);
-        }
-    };
+  return (
+    <header className="bg-barber-dark text-white shadow-md">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <Link href="/">
+            <img
+              src="/favicon.ico"
+              alt="Barber VIP Club"
+              className="h-10 w-10"
+            />
+          </Link>
+        </div>
 
+        {isMobile ? (
+          <>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={toggleMenu}
+              className="text-white"
+            >
+              {menuOpen ? <X size={24} /> : <Menu size={24} />}
+            </Button>
 
-    const toggleMenu = () => setMenuOpen(!menuOpen);
-
-    return (
-        <header className="bg-barber-dark text-white shadow-md">
-            <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                <div className="flex items-center space-x-2">
-                    <Link href="/">
-                        <img src="/favicon.ico" alt="Barber VIP Club" className="h-10 w-10" />
+            {menuOpen && (
+              <div className="absolute top-16 left-0 right-0 bg-barber-dark p-4 z-50 animate-fade-in">
+                <nav className="flex flex-col space-y-4">
+                  <Link
+                    href="/"
+                    className="text-white hover:text-barber-gold"
+                    onClick={toggleMenu}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/services"
+                    className="text-white hover:text-barber-gold"
+                    onClick={toggleMenu}
+                  >
+                    Barbearias
+                  </Link>
+                  <Link
+                    href="/appointments"
+                    className="text-white hover:text-barber-gold"
+                    onClick={toggleMenu}
+                  >
+                    Agendamentos
+                  </Link>
+                  <Link
+                    href="/membership"
+                    className="text-white hover:text-barber-gold"
+                    onClick={toggleMenu}
+                  >
+                    Clube VIP
+                  </Link>
+                  {/* {isAuthenticated ? ( */}
+                  <>
+                    <Link
+                      href="/dashboard"
+                      className="text-white hover:text-barber-gold"
+                      onClick={toggleMenu}
+                    >
+                      Painel
                     </Link>
-                </div>
+                    <Button variant="destructive" onClick={handleLogout}>
+                      Sair
+                    </Button>
+                  </>
+                  ) : (
+                  <>
+                    <Link
+                      href="/login"
+                      className="text-white hover:text-barber-gold"
+                      onClick={toggleMenu}
+                    >
+                      Login
+                    </Link>
+                    <Link href="/register">
+                      <Button
+                        variant="default"
+                        className="w-full bg-barber-gold hover:bg-barber-gold/90 text-black"
+                      >
+                        Registrar
+                      </Button>
+                    </Link>
+                  </>
+                  {/* )} */}
+                </nav>
+              </div>
+            )}
+          </>
+        ) : (
+          <div className="flex items-center space-x-6">
+            <nav className="flex space-x-6">
+              <Link
+                href="/homepage"
+                className="text-white hover:text-barber-gold"
+              >
+                Home
+              </Link>
+              <Link
+                href="/catalog"
+                className="text-white hover:text-barber-gold"
+              >
+                Barbearias
+              </Link>
+              <Link
+                href="/appointments"
+                className="text-white hover:text-barber-gold"
+              >
+                Agendamentos
+              </Link>
+              <Link
+                href="/membership"
+                className="text-white hover:text-barber-gold"
+              >
+                Clube VIP
+              </Link>
+              {/* {isAuthenticated && ( */}
+              <Link
+                href="/dashboard"
+                className="text-white hover:text-barber-gold"
+              >
+                Painel
+              </Link>
+              {/* )} */}
+            </nav>
 
-                {isMobile ? (
-                    <>
-                        <Button variant="ghost" size="icon" onClick={toggleMenu} className="text-white">
-                            {menuOpen ? <X size={24} /> : <Menu size={24} />}
-                        </Button>
-
-                        {menuOpen && (
-                            <div className="absolute top-16 left-0 right-0 bg-barber-dark p-4 z-50 animate-fade-in">
-                                <nav className="flex flex-col space-y-4">
-                                    <Link href="/" className="text-white hover:text-barber-gold" onClick={toggleMenu}>Home</Link>
-                                    <Link href="/services" className="text-white hover:text-barber-gold" onClick={toggleMenu}>Barbearias</Link>
-                                    <Link href="/appointments" className="text-white hover:text-barber-gold" onClick={toggleMenu}>Agendamentos</Link>
-                                    <Link href="/membership" className="text-white hover:text-barber-gold" onClick={toggleMenu}>Clube VIP</Link>
-                                    {isAuthenticated ? (
-                                        <>
-                                            <Link href="/dashboard" className="text-white hover:text-barber-gold" onClick={toggleMenu}>Painel</Link>
-                                            <Button variant="destructive" onClick={handleLogout}>Sair</Button>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <Link href="/login" className="text-white hover:text-barber-gold" onClick={toggleMenu}>Login</Link>
-                                            <Link href="/register">
-                                                <Button variant="default" className="w-full bg-barber-gold hover:bg-barber-gold/90 text-black">Registrar</Button>
-                                            </Link>
-                                        </>
-                                    )}
-                                </nav>
-                            </div>
-                        )}
-                    </>
-                ) : (
-                    <div className="flex items-center space-x-6">
-                        <nav className="flex space-x-6">
-                            <Link href="/homepage" className="text-white hover:text-barber-gold">Home</Link>
-                            <Link href="/catalog" className="text-white hover:text-barber-gold">Barbearias</Link>
-                            <Link href="/appointments" className="text-white hover:text-barber-gold">Agendamentos</Link>
-                            <Link href="/membership" className="text-white hover:text-barber-gold">Clube VIP</Link>
-                            {isAuthenticated && (
-                                <Link href="/dashboard" className="text-white hover:text-barber-gold">Painel</Link>
-                            )}
-                        </nav>
-
-                        <div className="flex items-center space-x-4">
-                            {isAuthenticated ? (
-                                <>
-                                    <Link href="/profile" className="text-white hover:text-barber-gold">
-                                        <UserCircle2 size={24} />
-                                    </Link>
-                                    <Button variant="outline" onClick={handleLogout} className="border-barber-gold text-barber-gold hover:bg-barber-gold hover:text-black">
-                                        Sair
-                                    </Button>
-                                </>
-                            ) : (
-                                <>
-                                    <Link href="/login">
-                                        <Button variant="ghost" className="text-white hover:text-barber-gold">Login</Button>
-                                    </Link>
-                                    <Link href="/register">
-                                        <Button variant="default" className="bg-barber-gold hover:bg-barber-gold/90 text-black">Registrar</Button>
-                                    </Link>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                )}
+            <div className="flex items-center space-x-4">
+              {/* {isAuthenticated ? ( */}
+              <>
+                <Link
+                  href="/profile"
+                  className="text-white hover:text-barber-gold"
+                >
+                  <UserCircle2 size={24} />
+                </Link>
+                <Button
+                  variant="outline"
+                  onClick={handleLogout}
+                  className="border-barber-gold text-barber-gold hover:bg-barber-gold hover:text-black"
+                >
+                  Sair
+                </Button>
+              </>
+              ) : (
+              <>
+                <Link href="/login">
+                  <Button
+                    variant="ghost"
+                    className="text-white hover:text-barber-gold"
+                  >
+                    Login
+                  </Button>
+                </Link>
+                <Link href="/register">
+                  <Button
+                    variant="default"
+                    className="bg-barber-gold hover:bg-barber-gold/90 text-black"
+                  >
+                    Registrar
+                  </Button>
+                </Link>
+              </>
+              {/* )} */}
             </div>
-        </header>
-    );
+          </div>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
